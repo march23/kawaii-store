28af84b7fdde8aa21dae9b9c95719f1aacb22d78
Marcela <marcela@Marcelas-MacBook-Pro.local>
2019-04-21 16:54:15 -0500

    update product's quantity

--- src/components/ListProduct.vue
+++ src/components/ListProduct.vue
@@ -2,7 +2,11 @@
     <ul class="list-group">
       <li class="list-group-item d-flex justify-content-between align-items-center" v-for="product in products">
           {{product.name}}
-          <span class="badge badge-primary badge-pill">{{product.quantity}}</span>
+          <span class="badge badge-primary badge-pill">
+            {{product.quantity}}
+            <button class="btn btn-primary btn-sm" type="button" @click="updateProductQuantity(product, 'increase')" >+</button>
+            <button class="btn btn-primary btn-sm" type="button" @click="updateProductQuantity(product, 'decrease')">-</button>
+          </span>
       </li>
     </ul>
 </template>
@@ -12,6 +16,11 @@
     name: 'ListProduct',
     props: {
       products: Array
+    },
+    methods: {
+      updateProductQuantity: function(product, type){
+        type == 'increase' ?  product.quantity++ : product.quantity--
+      }
     }
   }
 </script>
